158aa3ba224bd452636f07df93332128989b659e
Sven <svenrisse0@gmail.com>
2024-03-10 17:23:40 +0100

    feat: add create and view routes

--- main.go
+++ main.go
@@ -6,12 +6,26 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Hello from Snippetbox"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a snippet"))
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippte/create", snippetCreate)
 
 	log.Print("starting server on :4000")
 
