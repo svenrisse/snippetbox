43912f12d481bc8e5bca283f7795541481e4f42c
svenrisse <dev@svenrisse.com>
2024-03-13 10:55:14 +0100

    feat: add latest endpoint

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -17,6 +17,16 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	snippets, err := app.snippets.Latest()
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
+	for _, snippet := range snippets {
+		fmt.Fprintf(w, "%+v\n", snippet)
+	}
+
 	files := []string{
 		"./ui/html/base.tmpl.html",
 		"./ui/html/partials/nav.tmpl.html",
--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -36,17 +36,12 @@ func (m *SnippetModel) Insert(title string, content string, expires int) (int, e
 }
 
 func (m *SnippetModel) Get(id int) (Snippet, error) {
-	stmt := `SELECT id, title, content, expires FROM snippets 
-  WHERE expires > UTC_TIMESTAMP() AND id = ?`
-
-	row := m.DB.QueryRow(stmt, id)
-
 	var s Snippet
 
-	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Content, &s.Created, &s.Expires)
+	err := m.DB.QueryRow("SELECT id, title, content, expires FROM snippets WHERE expires > UTC_TIMESTAMP() AND id = ?`", id).Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
 	if err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
-			return Snipper{}, ErrNoRecord
+			return Snippet{}, ErrNoRecord
 		}
 		return Snippet{}, err
 	}
@@ -55,5 +50,30 @@ func (m *SnippetModel) Get(id int) (Snippet, error) {
 }
 
 func (m *SnippetModel) Latest() ([]Snippet, error) {
-	return nil, nil
+	stmt := `SELECET id, title, content, created, expires FROM snippets 
+  WHERE expires > UTC_TIMESTAMP() ORDER BY id DESC LIMIT 10`
+
+	rows, err := m.DB.Query(stmt)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var snippets []Snippet
+	for rows.Next() {
+		var s Snippet
+		err = rows.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+		if err != nil {
+			return nil, err
+		}
+
+		snippets = append(snippets, s)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return snippets, nil
 }
