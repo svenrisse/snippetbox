df5b6799e7e835ae6418375b531fac8c816dbe83
Sven <svenrisse0@gmail.com>
2024-03-14 17:26:18 +0100

    fix: sql queries and home end tag

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -20,7 +20,7 @@ type SnippetModel struct {
 
 func (m *SnippetModel) Insert(title string, content string, expires int) (int, error) {
 	stmt := `INSERT INTO snippets (title, content, created, expires)
-  VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
+    VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
 
 	result, err := m.DB.Exec(stmt, title, content, expires)
 	if err != nil {
@@ -36,33 +36,40 @@ func (m *SnippetModel) Insert(title string, content string, expires int) (int, e
 }
 
 func (m *SnippetModel) Get(id int) (Snippet, error) {
+	stmt := `SELECT id, title, content, created, expires FROM snippets
+    WHERE expires > UTC_TIMESTAMP() AND id = ?`
+
+	row := m.DB.QueryRow(stmt, id)
+
 	var s Snippet
 
-	err := m.DB.QueryRow("SELECT id, title, content, expires FROM snippets WHERE expires > UTC_TIMESTAMP() AND id = ?`", id).Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
 	if err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			return Snippet{}, ErrNoRecord
+		} else {
+			return Snippet{}, err
 		}
-		return Snippet{}, err
 	}
 
 	return s, nil
 }
 
 func (m *SnippetModel) Latest() ([]Snippet, error) {
-	stmt := `SELECET id, title, content, created, expires FROM snippets 
-  WHERE expires > UTC_TIMESTAMP() ORDER BY id DESC LIMIT 10`
+	stmt := `SELECT id, title, content, created, expires FROM snippets
+    WHERE expires > UTC_TIMESTAMP() ORDER BY id DESC LIMIT 10`
 
 	rows, err := m.DB.Query(stmt)
 	if err != nil {
 		return nil, err
 	}
-
 	defer rows.Close()
 
 	var snippets []Snippet
+
 	for rows.Next() {
 		var s Snippet
+
 		err = rows.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
 		if err != nil {
 			return nil, err
