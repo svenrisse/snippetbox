27a0a258fd2c22983e3cd02db24fe8ff5d691205
svenrisse <dev@svenrisse.com>
2024-03-15 08:10:52 +0100

    feat: add requestLogger middleware

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -15,3 +15,18 @@ func secureHeaders(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
+
+func (app *application) logRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var (
+			ip     = r.RemoteAddr
+			proto  = r.Proto
+			method = r.Method
+			uri    = r.URL.RequestURI()
+		)
+
+		app.logger.Info("received request", "ip", ip, "proto", proto, "method", method, "uri", uri)
+
+		next.ServeHTTP(w, r)
+	})
+}
