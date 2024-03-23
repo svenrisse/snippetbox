4b74b7ab5b0db95b13478892a943737a710cb4f9
Sven <svenrisse0@gmail.com>
2024-03-23 22:02:58 +0100

    fix: missing space

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -17,7 +17,7 @@ func (app *application) routes() http.Handler {
 	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
 	mux.Handle("GET /{$}", dynamic.ThenFunc(app.home))
 	mux.Handle("GET /snippet/view/{id}", dynamic.ThenFunc(app.snippetView))
-	mux.Handle("GET/user/signup", dynamic.ThenFunc(app.userSignup))
+	mux.Handle("GET /user/signup", dynamic.ThenFunc(app.userSignup))
 	mux.Handle("POST /user/signup", dynamic.ThenFunc(app.userSignupPost))
 	mux.Handle("GET /user/login", dynamic.ThenFunc(app.userLogin))
 	mux.Handle("POST /user/login", dynamic.ThenFunc(app.userLoginPost))
