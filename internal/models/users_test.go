f24f3ed82a23f2efc7d1a25707cbbfc593ec8c75
Sven <svenrisse0@gmail.com>
2024-03-23 22:10:25 +0100

    test: userExists integration test

---
+++ internal/models/users_test.go
@@ -0,0 +1,52 @@
+package models
+
+import (
+	"testing"
+
+	"github.com/svenrisse/snippetbox/internal/assert"
+)
+
+func TestUserModelExists(t *testing.T) {
+	// Set up a suite of table-driven tests and expected results.
+	tests := []struct {
+		name   string
+		userID int
+		want   bool
+	}{
+		{
+			name:   "Valid ID",
+			userID: 1,
+			want:   true,
+		},
+		{
+			name:   "Zero ID",
+			userID: 0,
+			want:   false,
+		},
+		{
+			name:   "Non-existent ID",
+			userID: 2,
+			want:   false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Call the newTestDB() helper function to get a connection pool to
+			// our test database. Calling this here -- inside t.Run() -- means
+			// that fresh database tables and data will be set up and torn down
+			// for each sub-test.
+			db := newTestDB(t)
+
+			// Create a new instance of the UserModel.
+			m := UserModel{db}
+
+			// Call the UserModel.Exists() method and check that the return
+			// value and error match the expected values for the sub-test.
+			exists, err := m.Exists(tt.userID)
+
+			assert.Equal(t, exists, tt.want)
+			assert.NilError(t, err)
+		})
+	}
+}
